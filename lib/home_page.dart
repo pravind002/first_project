c91882d95860db2f2a5039dd34c1acd2c0c9f2fc
pravind002 <parvindraj004@gmail.com>
2024-03-17 20:07:48 +0530

    added drawer,  assets

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:first_project/login_page.dart';
+import 'package:first_project/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,7 +9,51 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.greenAccent,
-      drawer: const Drawer(),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            const UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(),
+                accountName: Text('Pravind Raj'),
+                accountEmail: Text('pravind@gmail.com')),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                 Navigator.push(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const ProfilePage();
+                }));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.privacy_tip),
+              title: const Text('Privacy'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.help),
+              title: const Text('Help'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Setting'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Log Out'),
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const LoginPage();
+                }));
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: Colors.blue,
@@ -25,8 +71,12 @@ class HomePage extends StatelessWidget {
           Container(
             margin: const EdgeInsets.all(10),
             width: 500,
-            height: 500,
+            height: 200,
             color: Colors.purple,
+            child: Image.asset(
+              'assets/image.jpg',
+              fit: BoxFit.cover,
+            ),
           )
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:const HomePage(),
+      home: const HomePage(),
     );
   }
 }
